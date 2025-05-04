5c67d4f0d43f5fb9ef492a51a689c54be2c6ccaf
iamalshahed <shahedevise@gmail.com>
2025-05-05 00:59:12 +0600

    hero & nav dark mode enabled

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-/* 
+/*
  * @copywrite 2025 AL Shahed
- * @license Apache 2.0 
-*/
+ * @license Apache 2.0
+ */
 
 
+document.documentElement.classList.remove("dark");
+document.getElementById("switch").addEventListener("click", function () {
+  document.documentElement.classList.toggle("dark");
+});
 
-/* 
+/*
  * @copywrite 2025 AL Shahed
- * @license Apache 2.0 
-*/
+ * @license Apache 2.0
+ */
