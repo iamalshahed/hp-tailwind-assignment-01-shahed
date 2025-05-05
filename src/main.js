c80a35cca0d41f467c6d833564d29991ec495889
iamalshahed <shahedevise@gmail.com>
2025-05-05 23:53:51 +0600

    all buh fixed and add beautiful animation🙂🙂

--- src/main.js
+++ src/main.js
@@ -9,6 +9,41 @@ document.getElementById("switch").addEventListener("click", function () {
   document.documentElement.classList.toggle("dark");
 });
 
+
+
+// animation
+const animation = ScrollReveal({
+  distance: '30px',
+  duration: 1500,  
+  delay: 400,
+  reset: true
+});
+
+animation.reveal('.hero_content, .heroimage, .herobtn, .hero_reviews, .brooke', {
+  delay: 200,
+  origin: 'bottom'
+});
+
+animation.reveal('.brands, .five_courses, .footer', {
+  delay: 200,
+  origin: 'top'
+});
+
+animation.reveal('.ani_left', {
+  delay: 200,
+  origin: 'left'
+});
+
+animation.reveal('.ani_right', {
+  delay: 200,
+  origin: 'right'
+});
+
+
+
+
+
+
 /*
  * @copywrite 2025 AL Shahed
  * @license Apache 2.0
