c4bf8ff0a00050653793b237133d82bbf627a293
iamalshahed <shahedevise@gmail.com>
2025-05-09 19:10:02 +0600

    import lenis

--- src/main.js
+++ src/main.js
@@ -39,6 +39,16 @@ animation.reveal('.ani_right', {
   origin: 'right'
 });
 
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
+
 
 
 
